454b7c9e267f79c39b20220b8416d70c941ea617
Michel-Schnitker <Michel.Schnitker@gmx.de>
2022-10-05 00:06:10 +0200

    add first list tests

--- code/tests/src/listTest.c
+++ code/tests/src/listTest.c
@@ -13,27 +13,71 @@ void addAndGetTest(){
 
     print_info("    Running Tests for add to list and get from list ...");
 
-    //todo:
+    uint32_t originalNumber = 2;
 
-    print_warning("         Test not implement ...");
+    List *testList = new_List();
+
+    ifPrint_fatal(testList == null, "new_List() returned null");
+
+    testList->add(testList, &originalNumber);
+
+    ifPrint_fatal(testList->size != 1, "testList contains no Elements");
+
+    uint32_t *returnNumber = testList->getFirst(testList);
+
+    ifPrint_fatal(&originalNumber != returnNumber, "Pointer of original and return value is not the same");
+    ifPrint_fatal(testList->size != 1, "testList contains no Elements after get a Element");
+
+    testList->delete(testList, null);
+
+    print_success("         Passed");
 }
 
 void addAndPopTest(){
 
     print_info("    Running Tests for add to list and pop from list ...");
 
-    //todo:
+    uint32_t originalNumber = 2;
 
-    print_warning("         Test not implement ...");
+    List *testList = new_List();
+
+    testList->add(testList, &originalNumber);
+
+    ifPrint_fatal(testList->size != 1, "testList contains no Elements");
+
+    uint32_t *returnNumber = testList->popFirst(testList);
+
+    ifPrint_fatal(&originalNumber != returnNumber, "Pointer of original and return value is not the same");
+    ifPrint_fatal(testList->size != 0, "testList contains already elements");
+
+    testList->delete(testList, null);
+
+    print_success("         Passed");
 }
 
 void getIndexTest(){
 
     print_info("    Running Tests for get index from list ...");
 
-    //todo:
+    uint32_t originalNumberOne = 2;
+    uint32_t originalNumberTwo = 3;
 
-    print_warning("         Test not implement ...");
+    List *testList = new_List();
+
+    testList->add(testList, &originalNumberOne);
+    testList->add(testList, &originalNumberTwo);
+
+    ifPrint_fatal(testList->size != 2, "testList contains not enough Elements");
+
+    uint32_t *returnNumberTwo = testList->getIndex(testList, 1);
+    uint32_t *returnNumberOne = testList->getIndex(testList, 0);
+
+    ifPrint_fatal(&originalNumberOne != returnNumberOne or &originalNumberTwo != returnNumberTwo, "Pointer of original and return value is not the same");
+    ifPrint_fatal(testList->size != 2, "testList contains not enough elements after get");
+
+    testList->delete(testList, null);
+
+    print_success("         Passed");
 }
 
 void getCompareTest(){
@@ -49,9 +93,25 @@ void popIndexTest(){
 
     print_info("    Running Tests for pop index from list ...");
 
-    //todo:
+    uint32_t originalNumberOne = 2;
+    uint32_t originalNumberTwo = 3;
 
-    print_warning("         Test not implement ...");
+    List *testList = new_List();
+
+    testList->add(testList, &originalNumberOne);
+    testList->add(testList, &originalNumberTwo);
+
+    ifPrint_fatal(testList->size != 2, "testList contains not enough Elements");
+
+    uint32_t *returnNumberOne = testList->popIndex(testList, 0);
+    uint32_t *returnNumberTwo = testList->popIndex(testList, 0);
+
+    ifPrint_fatal(&originalNumberOne != returnNumberOne or &originalNumberTwo != returnNumberTwo, "Pointer of original and return value is not the same");
+    ifPrint_fatal(testList->size != 0, "testList contains to many elements after pop");
+
+    testList->delete(testList, null);
+
+    print_success("         Passed");
 }
 
 void popCompareTest(){

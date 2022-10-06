a36f2761128583962a5faa82b6fee5d3712924b0
Michel-Schnitker <Michel.Schnitker@gmx.de>
2022-10-06 13:40:06 +0200

    bugfix config not valid

--- code/tui/src/inputTui.c
+++ code/tui/src/inputTui.c
@@ -11,16 +11,22 @@ WINDOW* drawInputWindow(int startCol, int startRow, char *reqString){
     (void) startRow;
     (void) reqString;
 
+    //todo: draw window with request String
+
     return null;
 }
 
 void deleteInputWindow(WINDOW *window){
     (void) window;
+
+    //todo: delete Window
 }
 
 int32_t getIntegerFromInput(WINDOW *window, uint32_t maxDigits){
     (void) window;
     (void) maxDigits;
+
+    //todo: while numbers and no enter ... read numbers
 
     return 0;
 }
@@ -29,4 +35,6 @@ char* getStringFromInput(WINDOW *window){
     (void) window;
 
     UNREACHABLE;
+
+    //todo: needed for HighScore ...
 }
